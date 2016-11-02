479d18a5307218bf025720aff3ca688bd2cf6da1
Morgan Slimak <morganslimak@gmail.com>
2016-11-02 14:20:23 -0600

    add validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates_confirmation_of :password
+  validates_presence_of :first_name, :last_name
+  validates :username, uniqueness: true, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it "is valid with all fields" do
+      user = User.new(first_name: "morgan", last_name: "s", username: "user",
+                      password: "password", password_confirmation: "password")
+      expect(user).to be_valid
+    end
+
+    it "password confirmation works" do
+      user = User.new(first_name: "morgan", last_name: "s", username: "user",
+                      password: "password", password_confirmation: "pass")
+      expect(user).to be_invalid
+    end
+
+    it "username must be unique" do
+      user = User.create(first_name: "morgan", last_name: "s", username: "user",
+                      password: "password", password_confirmation: "password")
+      user2 = User.new(first_name: "morgan", last_name: "s", username: "user",
+                      password: "password", password_confirmation: "password")
+      expect(user2).to be_invalid
+    end
+
+    it "invalid without first_name" do
+      user = User.new(last_name: "s", username: "user",
+                      password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+
+    it "invalid without last_name" do
+      user = User.new(first_name: "morgan", username: "user",
+                      password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+
+    it "invalid without username" do
+      user = User.new(first_name: "morgan", last_name: "s",
+                      password: "password", password_confirmation: "password")
+      expect(user).to be_invalid
+    end
+  end
 end
