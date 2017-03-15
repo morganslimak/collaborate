1d6022f9be23c18faa76d8fba2aef083887e7dea
Morgan Slimak <morganslimak@gmail.com>
2017-03-15 17:43:33 -0400

    fix glitch that allows users to be added to projects more than once

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,11 +1,12 @@
 class MembersController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
-    if user.nil?
+    project = Project.find(params[:project_id])
+    if user.nil? || project.users.include?(user)
       #error
       redirect_to :back
     else
-      Member.create(user_id: user.id, project_id: params[:project_id])
+      Member.create(user_id: user.id, project_id: project.id)
       redirect_to :back
     end
   end
